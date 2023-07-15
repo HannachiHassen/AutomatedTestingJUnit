206303e8bba047b056efcc0e8450417b201aeb4c
HannachiHassen <hannachihassen07@gmail.com>
2023-07-15 16:44:35 -0400

    Create Calculator.java in main/java, CalculatorTest.java in test/java

---
+++ src/test/java/calculatorProject/CalculatorTest.java
@@ -0,0 +1,44 @@
+package calculatorProject;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public final class CalculatorTest {
+
+	public CalculatorTest() {
+	}
+	
+	Calculator calc;
+	
+	@Before
+	public void setUp() {
+		calc = new Calculator();
+	}
+	
+	@Test
+	public void addTwoNumbers() {
+		assertEquals(20, calc.add(10, 10));
+		
+	}
+
+	@Test
+	public void addThreeNumbers() {
+		assertEquals(20, calc.add(5, 5, 10));
+		
+	}
+
+	@Test
+	public void subtractTwoNumbers() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+
+	@Test
+	public void subtractThreeNumbers() {
+		assertEquals(0, calc.subtract(20, 10, 10));
+		
+	}
+
+}
